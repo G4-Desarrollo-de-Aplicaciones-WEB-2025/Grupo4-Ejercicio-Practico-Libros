0d7e04690b15f520e9d40b8eae74b7528b5173d9
MoiPad <moipad85@gmail.com>
2025-02-25 14:38:28 -0600

    update code

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Función para leer los libros desde el archivo
 function readBooks() {
     const data = fs.readFileSync("libros.json", "utf-8");
     return JSON.parse(data);
 }
 
+// Función para escribir los libros en el archivo
 function saveBooks(books) {
     fs.writeFileSync("libros.json", JSON.stringify(books, null, 2));
 }
 
-app.get("/libros", (req, res) => {
-    const libros = readBooks();
-    res.json(libros);
-});
+// endpoint GET para obtener libros
 
+
+
+// endpoint POST para agregar un libro nuevo
+
+
+
+//endpoint PUT para actualizar un libro 
 app.put("/libros/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const dataLibro = req.body;
@@ -36,6 +43,8 @@ app.put("/libros/:id", (req, res) => {
     }
 });
 
+
+
 app.listen(PORT, () => {
-    console.log(`Server activo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
